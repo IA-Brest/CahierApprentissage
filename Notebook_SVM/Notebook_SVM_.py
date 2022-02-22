ecb7f8b3d11fe7533c5cc93bfdbc8e404f98e579
Romain Heller <Rom1deTroyes@users.github.com>
2022-02-22 12:07:27 +0100

    +[SVM] add correction
    Signed-off-by: Romain Heller <Rom1deTroyes@users.github.com>

--- Notebook_SVM/Notebook_SVM_.py
+++ Notebook_SVM/Notebook_SVM_.py
@@ -37,12 +37,21 @@ df = pd.read_csv('accent-mfcc-data-1.csv')
 # %%
 df.head()
 
-
 # %% [markdown]
 # #### 2. Checker les variables quantitatives/qualitatives et les valeurs manquantes 
 
 # %%
 df.info()
+
+# %%
+df.shape
+
+# %%
+df.shape[0]
+len(df)
+
+# %%
+df.columns
 
 # %%
 # Quantitatives
@@ -51,6 +60,9 @@ df.describe()
 # %%
 # Qualitatives
 df.language.value_counts()
+
+# %%
+df.isnull().any()
 
 # %% [markdown]
 # #### 3. Visualiser les targets, et que remarquez vous ?
@@ -68,22 +80,22 @@ df.language.unique()
 # Fréquences de chaque valeurs
 pd.crosstab(df.language, "freq") #.plot(kind="hist")
 
-# %%
-
 # %% [markdown]
-#
-# #### 4. Notre variable target (Y) est 'language', Récuprer X et y à partir du jeu de données 
+# ##### #### 4. Notre variable target (Y) est 'language', Récuprer X et y à partir du jeu de données 
 
 # %%
 X = df.iloc[:,1:]  # type (X) -> DataFrame
-# X = df.language # type (X) -> Series
-# X = df["language"] # type (X) -> Series
+# X = df.language  # type (X) -> Series
+# X = df["language"]  # type (X) -> Series
+# X = df.select_dtypes('number')  # type (X) -> DataFrame
 X
 
 # %%
-y = df.iloc[:,:1] # type(y) -> DataFrame
-# y = df.iloc[:,0] # type(y) -> Series
-# y = df.select_dtypes('number') # type(y) -> DataFrame
+y = df.iloc[:,:1]  # type(y) -> DataFrame
+# y = df.iloc[:,0]  # type(y) -> Series
+# y = df.language  # type(y) -> Series
+# y = df['language']  # type(y) -> Series
+# y = df.select_dtypes('object') # type(y) -> DataFrame
 y
 
 
@@ -124,8 +136,8 @@ scaler.scale_
 # Appliquer la normalisation sur la base d'apprentissage et la base de test sans écraser les données brutes.
 
 # %%
-X_train_scaled = scaler.transform(X_test.copy())
-
+X_train_scaled = scaler.transform(X_train.copy())
+X_test_scaled = scaler.transform(X_test.copy())
 
 # %% [markdown]
 # Vérifier que la moyenne se rapproche à 0
@@ -164,6 +176,9 @@ X_train_scaled.std()
 # Quel est votre proposition pour ce client ?
 
 # %%
+# 2 possibilité :
+# - Supprimer les classes non utilisées
+# - Grouper les classes non utilisées en une seule classe commune
 
 # %% [markdown]
 # J'ai programmé une gonction pour vous faciliter la classification d'un nouveau Voice, la fonction est "rec_accent".
